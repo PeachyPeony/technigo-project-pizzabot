cef9133c60d38d94e535fe8f614bd4308c7e825b
Alicia <allypally_x@hotmail.com>
2026-09-03 12:08:50 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -12,7 +12,22 @@ alert(`Hello and welcome, ${userName}!`);
 
 // Step 2 - Food choice
 // Your code goes here
+let foodChoice = prompt("What type of food would you like to order?\n" +
+  "1. Pizza\n" +
+  "2. Pasta\n" +
+  "3. Salad");
 
+let foodType;
+
+if (foodChoice == 1) {
+  foodType = "Pizza";
+} else if (foodChoice == 2) {
+  foodType = "Pasta";
+} else if (foodChoice == 3) {
+  foodType = "Salad";
+} else {
+  alert("Invalid choice.");
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
