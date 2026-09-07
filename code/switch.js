93e557dd617de802b2c123c4f5cdd29d246d5fec
Alicia <allypally_x@hotmail.com>
2026-09-07 17:52:56 +0200

    Create switch.js
    Add step 1 to switch.js

---
+++ code/switch.js
@@ -0,0 +1,8 @@
+// Step 1 - Welcome and introduction
+alert(
+    `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+)
+
+let userName = prompt("What is your name?");
+
+alert(`Hello and welcome, ${userName}!`);
